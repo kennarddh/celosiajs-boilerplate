a9c4268c8636c0bd059fd0a6e24e61e2f1a34506
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-26 20:35:39 +0700

    feat: Env rate limiter

--- src/Middlewares/RateLimiter.js
+++ src/Middlewares/RateLimiter.js
@@ -1,8 +1,8 @@
 import rateLimit from 'express-rate-limit'
 
 const RateLimiter = rateLimit({
-	windowMs: 1 * 60 * 1000, // 1 minutes
-	max: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
+	windowMs: process.env.RATE_LIMITER_WINDOW_MS || 1 * 60 * 1000, // 1 minutes
+	max: process.env.RATE_LIMITER_MAX || 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
 	legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
 	message: {
