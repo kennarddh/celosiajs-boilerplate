2be146903bd8f1b8476a4c43664ca15c3ec74ae4
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-26 18:59:22 +0700

    feat: Login controller

--- src/Controllers/Auth.js
+++ src/Controllers/Auth.js
@@ -5,7 +5,7 @@ import bcrypt from 'bcrypt'
 import User from '../Models/User'
 
 export const Register = async (req, res) => {
-	const { username, name, email, password } = req
+	const { username, name, email, password } = req.body
 
 	const user = new User({
 		username: username.toLowerCase(),
@@ -27,6 +27,55 @@ export const Register = async (req, res) => {
 			return res.status(500).json({
 				success: false,
 				error: 'User not created',
+			})
+		})
+}
+
+export const Login = (req, res) => {
+	const { email, password } = req.body
+
+	User.findOne({ email })
+		.exec()
+		.then(user => {
+			if (!user) {
+				return res.status(403).json({
+					success: false,
+					error: 'Invalid email or password',
+				})
+			}
+
+			bcrypt.compare(password, user.password).then(isPasswordCorrect => {
+				if (!isPasswordCorrect) {
+					return res.status(403).json({
+						success: false,
+						error: 'Invalid email or password',
+					})
+				}
+
+				const payload = {
+					id: user._id,
+					username: user.username,
+				}
+
+				jwt.sign(
+					payload,
+					process.env.JWT_SECRET,
+					{ expiresIn: 86400 },
+					(error, token) => {
+						if (error)
+							return res.status(500).json({
+								success: false,
+								error: 'Login Failed',
+							})
+
+						return res.status(200).json({
+							success: true,
+							data: {
+								token: `Bearer ${token}`,
+							},
+						})
+					}
+				)
 			})
 		})
 }
