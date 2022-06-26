074034c49d620a12b8ecba3548ccaea1347cd9fb
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-26 19:19:42 +0700

    feat: Validate auth

--- src/Routes/Auth.js
+++ src/Routes/Auth.js
@@ -6,10 +6,14 @@ import { Register, Login, GetUserData } from '../Controllers/Auth'
 // Middleware
 import VerifyJWT from '../Middlewares/VerifyJWT'
 
+// Validation
+import RegisterValidation from '../Validation/Authentication/Register'
+import LoginValidation from '../Validation/Authentication/Login'
+
 const Router = express.Router()
 
-Router.post('/auth/register', Register)
-Router.post('/auth/login', Login)
+Router.post('/auth/register', RegisterValidation(), Register)
+Router.post('/auth/login', LoginValidation(), Login)
 Router.get('/auth/user', VerifyJWT, GetUserData)
 
 export default Router
