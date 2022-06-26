846fee68799cdd13ee1a4583d7f062287f927888
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-26 20:24:39 +0700

    feat: Change rate limiter response

--- src/App.js
+++ src/App.js
@@ -31,7 +31,11 @@ const limiter = rateLimit({
 	windowMs: 1 * 60 * 1000, // 1 minutes
 	max: 100, // Limit each IP to 100 requests per `window` (here, per 1 minutes)
 	standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
-	legacyHeaders: false, // Disable the `X-RateLimit-*` headers
+	legacyHeaders: false, // Disable the `X-RateLimit-*` headers,
+	message: {
+		success: false,
+		message: 'Too many requests, please try again later.',
+	},
 })
 
 app.use(limiter)
