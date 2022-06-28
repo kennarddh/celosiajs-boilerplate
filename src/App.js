e412e989ba1d604208d443198392fb266c4204d7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-06-28 11:37:11 +0700

    feat: Sanitive mongo middleware

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import cors from 'cors'
 import compression from 'compression'
 import helmet from 'helmet'
 import RateLimiter from './Middlewares/RateLimiter'
+import MongoSanitize from './Middlewares/MongoSanitize'
 
 // Database
 import db from './Database'
@@ -28,6 +29,8 @@ app.use(express.json())
 app.use(cors())
 
 app.use(RateLimiter)
+
+app.use(MongoSanitize)
 
 // Database
 db.on('error', error => {
