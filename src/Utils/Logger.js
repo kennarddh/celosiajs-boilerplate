c7850ca9d5ef8f6a1bb832f4335ce890d2e823df
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-03 17:37:37 +0700

    feat: Log to file

--- src/Utils/Logger.js
+++ src/Utils/Logger.js
@@ -2,7 +2,12 @@ import winston from 'winston'
 
 const Logger = winston.createLogger({
 	level: process.env.LOG_LEVEL || 'info',
-	transports: [new winston.transports.Console({})],
+	transports: [
+		new winston.transports.Console({}),
+		new winston.transports.File({
+			filename: 'Logs/Application.log',
+		}),
+	],
 	format: winston.format.combine(
 		winston.format.timestamp(),
 		winston.format.metadata(),
