af4055cec3156cf4233212828f82ce2013a71aab
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-03 17:32:18 +0700

    feat: Logger format

--- src/Utils/Logger.js
+++ src/Utils/Logger.js
@@ -3,6 +3,12 @@ import winston from 'winston'
 const Logger = winston.createLogger({
 	level: process.env.LOG_LEVEL || 'info',
 	transports: [new winston.transports.Console({})],
+	format: winston.format.combine(
+		winston.format.timestamp(),
+		winston.format.metadata(),
+		winston.format.ms(),
+		winston.format.json()
+	),
 })
 
 export default Logger
