dbdc77eb7b4396d1eca7e3cb126707d5f7c34b5c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-04 14:51:53 +0700

    fix: Remove debug log in production

--- src/Utils/Logger/Logger.js
+++ src/Utils/Logger/Logger.js
@@ -22,45 +22,45 @@ const LoggerFormat = [
 	winston.format.json(),
 ]
 
-const Logger = winston.createLogger({
-	level: process.env.LOG_LEVEL || 'info',
-	silent: process.env.NODE_ENV === 'test',
-	format: winston.format.combine(...LoggerFormat),
-	transports: [
-		new winston.transports.Console({
-			silent: process.env.NODE_ENV !== 'development',
-		}),
-		new WinstonDailyRotateFile({
-			dirname: path.resolve(logsRootDirectory, 'Application'),
-			filename: 'Application-%DATE%.log',
-			zippedArchive: true,
-			maxSize: '1m',
-			maxFiles: '14d',
-		}),
-		new WinstonDailyRotateFile({
-			dirname: path.resolve(logsRootDirectory, 'Error'),
-			level: 'error',
-			filename: 'Error.log-%DATE%',
-			zippedArchive: true,
-			maxSize: '1m',
-			maxFiles: '14d',
-			handleExceptions: true,
-			handleRejections: true,
-		}),
-		new WinstonDailyRotateFile({
-			dirname: path.resolve(logsRootDirectory, 'Http'),
-			level: 'http',
-			filename: 'Http.log-%DATE%',
-			zippedArchive: true,
-			maxSize: '1m',
-			maxFiles: '14d',
-			format: winston.format.combine(
-				RemoveError(),
-				RemoveWarn(),
-				RemoveInfo(),
-				...LoggerFormat
-			),
-		}),
+const Transports = [
+	new winston.transports.Console({
+		silent: process.env.NODE_ENV !== 'development',
+	}),
+	new WinstonDailyRotateFile({
+		dirname: path.resolve(logsRootDirectory, 'Application'),
+		filename: 'Application-%DATE%.log',
+		zippedArchive: true,
+		maxSize: '1m',
+		maxFiles: '14d',
+	}),
+	new WinstonDailyRotateFile({
+		dirname: path.resolve(logsRootDirectory, 'Error'),
+		level: 'error',
+		filename: 'Error.log-%DATE%',
+		zippedArchive: true,
+		maxSize: '1m',
+		maxFiles: '14d',
+		handleExceptions: true,
+		handleRejections: true,
+	}),
+	new WinstonDailyRotateFile({
+		dirname: path.resolve(logsRootDirectory, 'Http'),
+		level: 'http',
+		filename: 'Http.log-%DATE%',
+		zippedArchive: true,
+		maxSize: '1m',
+		maxFiles: '14d',
+		format: winston.format.combine(
+			RemoveError(),
+			RemoveWarn(),
+			RemoveInfo(),
+			...LoggerFormat
+		),
+	}),
+]
+
+if (process.env.NODE_ENV === 'development') {
+	Transports.push(
 		new WinstonDailyRotateFile({
 			dirname: path.resolve(logsRootDirectory, 'Debug'),
 			level: 'debug',
@@ -70,8 +70,15 @@ const Logger = winston.createLogger({
 			maxFiles: '14d',
 			format: winston.format.combine(RemoveHttp(), ...LoggerFormat),
 			silent: process.env.NODE_ENV !== 'development',
-		}),
-	],
+		})
+	)
+}
+
+const Logger = winston.createLogger({
+	level: process.env.LOG_LEVEL || 'info',
+	silent: process.env.NODE_ENV === 'test',
+	format: winston.format.combine(...LoggerFormat),
+	transports: Transports,
 })
 
 export default Logger
