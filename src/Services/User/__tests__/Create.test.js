6927e413f7f07891fd17edeedc2db9da3c511c71
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-05 22:42:39 +0700

    test: Create user service

---
+++ src/Services/User/__tests__/Create.test.js
@@ -0,0 +1,29 @@
+import * as mockingoose from 'mockingoose'
+
+import Create from '../Create'
+
+// Models
+import User from '../../../Models/User'
+
+describe('Create user service', () => {
+	it('Should create new user', () => {
+		expect.assertions(3)
+
+		const user = {
+			username: 'username',
+			name: 'Name',
+			email: 'email@example.com',
+			password: 'password',
+		}
+
+		mockingoose(User, 'save').toReturn(user)
+
+		const createPromise = Create(user).then(({ user: newUser }) => {
+			expect(newUser.username).toBe(user.username)
+			expect(newUser.name).toBe(user.name)
+			expect(newUser.email).toBe(user.email)
+		})
+
+		return createPromise
+	})
+})
