5b8e3b7294eac7cf8edd12360dabee6ee785cccc
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-05 13:54:53 +0700

    feat: Find by id user service

--- src/Services/User/FindById.js
+++ src/Services/User/FindById.js
@@ -3,15 +3,15 @@ import User from '../../Models/User'
 
 import Logger from '../../Utils/Logger/Logger'
 
-const FindByEmail = ({ email }) =>
+const FindByEmail = ({ id }) =>
 	new Promise((resolve, reject) => {
-		User.findOne({ email: email.toLowerCase() })
+		User.findById(id)
 			.exec()
 			.then(user => {
 				resolve({ user })
 			})
 			.catch(error => {
-				Logger.error('Find by email failed user service', {
+				Logger.error('Find by id failed user service', {
 					error,
 				})
 
