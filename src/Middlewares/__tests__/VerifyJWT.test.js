6732cd4313bed8db9b7ae2d4f6b2c470074312bb
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-06 23:06:19 +0700

    test: Verify jwt middleware success

---
+++ src/Middlewares/__tests__/VerifyJWT.test.js
@@ -0,0 +1,54 @@
+import VerifyJWT from '../VerifyJWT'
+
+const user = {
+	id: 'id',
+	username: 'username',
+}
+
+jest.mock('../../Utils/Promises/JWTVerify', () => ({
+	__esModule: true,
+	default: jest.fn().mockResolvedValue(user),
+}))
+
+describe('Verify JWT middleware', () => {
+	const nextFunction = jest.fn()
+	const jsonFunction = jest.fn()
+	const statusFunction = jest.fn(() => ({
+		json: jsonFunction,
+	}))
+
+	let mockRequest
+	let mockResponse
+
+	beforeEach(() => {
+		mockRequest = {}
+		mockResponse = {
+			status: statusFunction,
+		}
+	})
+
+	it('Should success', async () => {
+		expect.assertions(3)
+		mockRequest = {
+			headers: {
+				'x-access-token':
+					'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzQ1N2RmNjhhZDc0NTRjNTRhNWFjOSIsInVzZXJuYW1lIjoidGVzdHRlc3QxMiIsImlhdCI6MTY1NzEwMTIzMSwiZXhwIjoxNjU3MTg3NjMxfQ.-YlQ95KUSFaxGZLvTlQLkAEkXBLHmSqzIeJspzSw5vM',
+			},
+		}
+
+		const promise = new Promise(resolve => {
+			VerifyJWT(mockRequest, mockResponse, () => {
+				nextFunction()
+
+				resolve()
+			})
+		}).then(() => {
+			expect(nextFunction).toHaveBeenCalled()
+
+			expect(mockRequest.user.id).toBe(user.id)
+			expect(mockRequest.user.username).toBe(user.username)
+		})
+
+		return promise
+	})
+})
