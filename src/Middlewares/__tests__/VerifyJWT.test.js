312860fbe42f830c29152423218cf30f557040a9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-07 11:40:38 +0700

    test: Should failed with invalid token verify jwt

--- src/Middlewares/__tests__/VerifyJWT.test.js
+++ src/Middlewares/__tests__/VerifyJWT.test.js
@@ -1,3 +1,5 @@
+import JWTVerify from '../../Utils/Promises/JWTVerify'
+
 import VerifyJWT from '../VerifyJWT'
 
 const user = {
@@ -52,7 +54,46 @@ describe('Verify JWT middleware', () => {
 		return promise
 	})
 
-	it.todo('Should failed with invalid token')
+	it('Should failed with invalid token', async () => {
+		expect.assertions(4)
+		mockRequest = {
+			headers: {
+				'x-access-token':
+					'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjYyYzQ1N2RmNjhhZDc0NTRjNTRhNWFjOSIsInVzZXJuYW1lIjoidGVzdHRlc3QxMiIsImlhdCI6MTY1NzEwMTIzMSwiZXhwIjoxNjU3MTg3NjMxfQ.-YlQ95KUSFaxGZLvTlQLkAEkXBLHmSqzIeJspzSw5vM',
+			},
+		}
+
+		JWTVerify.mockRejectedValueOnce({
+			success: false,
+			error: 'Failed to authenticate',
+		})
+
+		const promise = new Promise((_, reject) => {
+			VerifyJWT(mockRequest, mockResponse, () => {
+				nextFunction()
+
+				reject()
+			})
+		})
+
+		const mock = jest.fn()
+
+		try {
+			await promise()
+		} catch {
+			mock()
+		}
+
+		expect(mock).toHaveBeenCalled()
+		expect(nextFunction).toHaveBeenCalled()
+
+		expect(jsonFunction.mock.calls[0][0]).toEqual({
+			success: false,
+			error: 'Failed to authenticate',
+		})
+
+		expect(statusFunction.mock.calls[0][0]).toBe(401)
+	})
 
 	it.todo('Should failed with invalid header')
 
