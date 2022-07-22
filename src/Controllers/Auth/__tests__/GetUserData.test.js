69bddd18129bd829a3630406a27b8c32ab68cb38
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-22 14:21:31 +0700

    test: Get user data success

---
+++ src/Controllers/Auth/__tests__/GetUserData.test.js
@@ -0,0 +1,62 @@
+import request from 'supertest'
+
+import FindById from '../../../Services/User/FindById'
+import JWTVerify from '../../../Utils/Promises/JWTVerify'
+
+import App from '../../../App'
+
+jest.mock('../../../Services/User/FindById')
+jest.mock('../../../Utils/Promises/JWTVerify')
+
+describe('Get user data', () => {
+	let user
+
+	beforeEach(() => {
+		user = {
+			_id: '1234',
+			username: 'testtest1234',
+			name: 'Testtest1234',
+			email: 'testtest1234@gmail.com',
+			password: 'testtest1234',
+		}
+	})
+
+	afterEach(() => {
+		jest.clearAllMocks()
+		jest.restoreAllMocks()
+		jest.resetModules()
+	})
+
+	it('Should success', async () => {
+		expect.assertions(5)
+
+		const token = 'Bearer token'
+
+		JWTVerify.mockResolvedValueOnce({
+			id: user._id,
+			username: user.username,
+		})
+
+		FindById.mockResolvedValueOnce({ user })
+
+		const res = await request(App)
+			.get('/api/auth/user')
+			.set('x-access-token', token)
+
+		expect(JWTVerify).toHaveBeenCalledWith('token', undefined)
+
+		expect(FindById).toHaveBeenCalledWith({ id: user._id })
+
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toHaveProperty('data')
+		expect(res.body).toEqual({
+			success: true,
+			data: {
+				id: user._id,
+				username: user.username,
+				name: user.name,
+				email: user.email,
+			},
+		})
+	})
+})
