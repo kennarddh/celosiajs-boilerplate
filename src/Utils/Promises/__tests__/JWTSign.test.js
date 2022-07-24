1eadf2e5c51aec49c239cdb01e47f169280232c7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-25 00:52:29 +0700

    test: JWT sign failed

--- src/Utils/Promises/__tests__/JWTSign.test.js
+++ src/Utils/Promises/__tests__/JWTSign.test.js
@@ -30,6 +30,7 @@ describe('JWT sign', () => {
 				callback
 			) => {
 				callback(null, 'token')
+
 				mock(
 					payloadImplementation,
 					secretImplementation,
@@ -44,5 +45,48 @@ describe('JWT sign', () => {
 
 		expect(mock).toHaveBeenCalledTimes(1)
 		expect(mock).toBeCalledWith(payload, secret, options)
+	})
+
+	it('Should failed', async () => {
+		expect.assertions(3)
+
+		const payload = {
+			foo: 'bar',
+		}
+
+		const secret = 'secret'
+
+		const options = {
+			expiresIn: 10,
+		}
+
+		const sign = jest.spyOn(jwt, 'sign')
+
+		const mock = jest.fn()
+		const mock2 = jest.fn()
+
+		sign.mockImplementation(
+			(
+				payloadImplementation,
+				secretImplementation,
+				optionsImplementation,
+				callback
+			) => {
+				callback(new Error('error'), null)
+
+				mock(
+					payloadImplementation,
+					secretImplementation,
+					optionsImplementation
+				)
+			}
+		)
+
+		await JWTSign(payload, secret, options).catch(mock2)
+
+		expect(mock).toHaveBeenCalledTimes(1)
+		expect(mock).toBeCalledWith(payload, secret, options)
+
+		expect(mock2).toBeCalledWith(new Error('error'))
 	})
 })
