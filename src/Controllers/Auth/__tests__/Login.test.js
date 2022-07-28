11934f909f5dae3f66e8b26a0fe6ee003d703094
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-28 15:11:32 +0700

    test: Should fail with failed refresh token jwt sign login

--- src/Controllers/Auth/__tests__/Login.test.js
+++ src/Controllers/Auth/__tests__/Login.test.js
@@ -114,6 +114,33 @@ describe('Login', () => {
 		})
 	})
 
+	it('Should fail with failed refresh token jwt sign', async () => {
+		FindByEmail.mockResolvedValueOnce({
+			user: {
+				password: 'testtest',
+				_id: 'id',
+				username: 'testtest',
+			},
+		})
+
+		bcrypt.compare.mockResolvedValueOnce(true)
+
+		JWTSign.mockResolvedValueOnce('token')
+		JWTSign.mockRejectedValueOnce('error')
+
+		const res = await request(App).post('/api/auth/login').send({
+			email: 'test@test.com',
+			password: 'testtest',
+		})
+
+		expect(res.statusCode).toEqual(500)
+		expect(res.body).toHaveProperty('error')
+		expect(res.body).toEqual({
+			success: false,
+			error: 'Internal server error',
+		})
+	})
+
 	it('Should fail with invalid password', async () => {
 		expect.assertions(3)
 		FindByEmail.mockResolvedValueOnce({
