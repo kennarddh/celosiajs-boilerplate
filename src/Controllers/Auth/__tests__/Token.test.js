cba5fa0594fdd6b2a4bcd7c81a10597b30b2538c
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-07-28 15:45:31 +0700

    test: Token success

---
+++ src/Controllers/Auth/__tests__/Token.test.js
@@ -0,0 +1,56 @@
+import request from 'supertest'
+
+import JWTSign from '../../../Utils/Promises/JWTSign'
+import JWTVerify from '../../../Utils/Promises/JWTVerify'
+
+import App from '../../../App'
+
+jest.mock('../../../Utils/Promises/JWTSign')
+jest.mock('../../../Utils/Promises/JWTVerify')
+
+describe('Token', () => {
+	afterEach(() => {
+		jest.clearAllMocks()
+		jest.restoreAllMocks()
+		jest.resetModules()
+
+		JWTSign.mockRestore()
+		JWTVerify.mockRestore()
+	})
+
+	it('Should success', async () => {
+		expect.assertions(7)
+
+		const payload = {
+			id: 'id',
+			username: 'username',
+		}
+
+		JWTVerify.mockResolvedValueOnce(payload)
+
+		JWTSign.mockResolvedValueOnce('token')
+		JWTSign.mockResolvedValueOnce('refreshToken')
+
+		const res = await request(App)
+			.post('/api/auth/token')
+			.set('Cookie', ['refreshToken=refreshToken'])
+
+		expect(JWTVerify.mock.calls[0][0]).toBe('refreshToken')
+
+		expect(JWTSign.mock.calls[0][0]).toStrictEqual(payload)
+		expect(JWTSign.mock.calls[1][0]).toStrictEqual(payload)
+
+		expect(res.statusCode).toEqual(200)
+		expect(res.body).toHaveProperty('data')
+		expect(res.body).toEqual({
+			success: true,
+			data: {
+				token: `Bearer token`,
+			},
+		})
+
+		expect(res.header['set-cookie']).toEqual([
+			'refreshToken=refreshToken; Path=/; HttpOnly; SameSite=Lax',
+		])
+	})
+})
