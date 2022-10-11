5aac0f832811577c257abf916a7696498f424856
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-11 16:32:47 +0700

    chore: Create swagger file on prepare

--- src/Routes/Docs.js
+++ src/Routes/Docs.js
@@ -2,7 +2,7 @@ import express from 'express'
 
 import swaggerUi from 'swagger-ui-express'
 
-import swaggerDocument from '../Swagger/Swagger'
+import swaggerDocument from '../Swagger'
 
 const Router = express.Router()
 
