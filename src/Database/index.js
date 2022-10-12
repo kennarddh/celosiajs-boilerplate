95e16b162e50887c82190b67a3cf39e34f9eecad
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-12 14:12:56 +0700

    refactor: Move monggodb error event to db module

--- src/Database/index.js
+++ src/Database/index.js
@@ -5,9 +5,13 @@ import Logger from '../Utils/Logger/Logger'
 mongoose
 	.connect(process.env.DB_HOST, { useNewUrlParser: true })
 	.catch(error => {
-		Logger.error('Mongo DB connection error', { error })
+		Logger.error('Mongo DB connect error', { error })
 	})
 
 const db = mongoose.connection
+
+db.on('error', error => {
+	Logger.error('Mongo DB connection error', { error })
+})
 
 export default db
