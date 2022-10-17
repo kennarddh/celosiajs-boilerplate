61ccf0d0a4b61cba86964f25d9693f1cf68c0543
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-17 14:46:56 +0700

    feat: Declare env types

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,15 @@
 declare global {
 	namespace NodeJS {
 		interface ProcessEnv {
-			PORT: number
+			PORT: string
+			DB_HOST: string
+			JWT_SECRET: string
+			REFRESH_JWT_SECRET: string
+			JWT_EXPIRE: string
+			REFRESH_JWT_EXPIRE: string
+			RATE_LIMITER_MAX: string
+			RATE_LIMITER_WINDOW_MS: string
+			LOG_LEVEL: string
 		}
 	}
 }
