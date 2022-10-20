be32da65958fb895c45c018e9595cc53b2b20c9f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2022-10-20 20:32:37 +0700

    test: Find by id user service ts

--- src/Services/User/__tests__/FindById.test.ts
+++ src/Services/User/__tests__/FindById.test.ts
@@ -1,6 +1,6 @@
-import * as mockingoose from 'mockingoose'
-
 import mongoose from 'mongoose'
+
+import MockMongoose, { ResetAll } from '../../../Utils/Tests/MockMongoose'
 
 import FindById from '../FindById'
 
@@ -9,24 +9,24 @@ import User from '../../../Models/User'
 
 describe('Find by id user service', () => {
 	afterEach(() => {
-		mockingoose.resetAll()
+		ResetAll()
 	})
 
 	it('Should get user', () => {
 		expect.assertions(6)
 
 		const user = {
-			_id: mongoose.Types.ObjectId('62c526bb503a77b155f6eba5'),
+			_id: new mongoose.Types.ObjectId('62c526bb503a77b155f6eba5'),
 			username: 'username',
 			name: 'Name',
 			email: 'email@example.com',
 			password: 'password',
 		}
 
-		mockingoose(User).toReturn(query => {
-			expect(query.getQuery()._id).toBe(user._id)
+		MockMongoose(User).toReturn(query => {
+			expect(query._id).toBe(user._id)
 
-			if (query.getQuery()._id === user._id) return user
+			if (query._id === user._id) return user
 		}, 'findOne')
 
 		const findByIdPromise = FindById({ id: user._id }).then(
@@ -46,26 +46,26 @@ describe('Find by id user service', () => {
 		expect.assertions(2)
 
 		const user = {
-			_id: mongoose.Types.ObjectId('62c526bb503a77b155f6eba6'),
+			_id: new mongoose.Types.ObjectId('62c526bb503a77b155f6eba6'),
 			username: 'username1',
 			name: 'Name1',
 			email: 'email1@example.com',
 			password: 'password1',
 		}
 
-		mockingoose(User).toReturn(query => {
-			expect(query.getQuery()._id).toEqual(
-				mongoose.Types.ObjectId('62c526bb503a77b155f6eba5')
+		MockMongoose(User).toReturn(query => {
+			expect(query._id).toEqual(
+				new mongoose.Types.ObjectId('62c526bb503a77b155f6eba5')
 			)
 
-			if (query.getQuery()._id === user._id) return user
+			if (query._id === user._id) return user
 		}, 'findOne')
 
 		const mock = jest.fn()
 
 		try {
 			await FindById({
-				id: mongoose.Types.ObjectId('62c526bb503a77b155f6eba5'),
+				id: new mongoose.Types.ObjectId('62c526bb503a77b155f6eba5'),
 			})
 		} catch ({ code }) {
 			mock(code)
@@ -77,13 +77,13 @@ describe('Find by id user service', () => {
 	it('Should reject with 500', async () => {
 		expect.assertions(1)
 
-		mockingoose(User).toReturn(new Error(), 'findOne')
+		MockMongoose(User).toReturn(new Error(), 'findOne')
 
 		const mock = jest.fn()
 
 		try {
 			await FindById({
-				id: mongoose.Types.ObjectId('62c526bb503a77b155f6eba5'),
+				id: new mongoose.Types.ObjectId('62c526bb503a77b155f6eba5'),
 			})
 		} catch ({ code }) {
 			mock(code)
