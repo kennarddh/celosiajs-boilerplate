1e2b044eea0f1e4a8f29d91d9ee5ac11dbad84c9
kennarddh <79390992+kennarddh@users.noreply.github.com>
2023-04-21 00:43:37 +0700

    test: On shutdown string port

--- src/Utils/OnShutdown/OnShutdown.test.ts
+++ src/Utils/OnShutdown/OnShutdown.test.ts
@@ -56,4 +56,46 @@ describe('On shutdown', () => {
 		expect(databaseSpy).toHaveBeenCalledTimes(1)
 		expect(databaseSpy.mock.calls[0]?.[0]).toBe(false)
 	})
+
+	it('Should success with string port', async () => {
+		expect.assertions(8)
+
+		const loggerSpy = jest.spyOn(Logger, 'info')
+		const databaseSpy = jest.spyOn(Database, 'close')
+		const processSpy = jest.spyOn(process, 'exit')
+
+		databaseSpy.mockResolvedValue()
+
+		const close = jest.fn()
+		const exit = jest.fn()
+
+		processSpy.mockImplementation((code?: number) => {
+			exit(code)
+			return undefined as never
+		})
+
+		await OnShutdown(
+			{
+				close: callback => {
+					callback?.()
+					close()
+				},
+			} as Server,
+			'8080',
+			'SIGINT'
+		)()
+
+		expect(processSpy).toHaveBeenCalledTimes(1)
+		expect(processSpy).toHaveBeenCalledWith(0)
+
+		expect(loggerSpy).toHaveBeenCalledTimes(3)
+		expect(loggerSpy.mock.calls[0]?.[0]).toBe(
+			'SIGINT signal received: Stopping server'
+		)
+		expect(loggerSpy.mock.calls[1]?.[0]).toBe('Server Stopped')
+		expect(loggerSpy.mock.calls[2]?.[0]).toBe('Database connection closed')
+
+		expect(databaseSpy).toHaveBeenCalledTimes(1)
+		expect(databaseSpy.mock.calls[0]?.[0]).toBe(false)
+	})
 })
