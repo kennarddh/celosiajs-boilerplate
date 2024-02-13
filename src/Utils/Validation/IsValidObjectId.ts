fe565d52a84de3db74ede5d21b3f69e93b3a5652
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-13 13:38:31 +0700

    feat: Remove object id validator

--- src/Utils/Validation/IsValidObjectId.ts
+++
@@ -1,9 +0,0 @@
-import { Types } from 'mongoose'
-
-const IsValidObjectId = (value: string) => {
-	if (Types.ObjectId.isValid(value)) return true
-
-	return false
-}
-
-export default IsValidObjectId
