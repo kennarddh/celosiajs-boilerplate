3174a5e7687f31bf7b23306089bc1b2aadb981c6
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-13 16:04:44 +0700

    feat: Handle prisma connect error

--- src/Database/index.ts
+++ src/Database/index.ts
@@ -1,9 +1,20 @@
 import { PrismaClient } from '@prisma/client'
+import { PrismaClientInitializationError } from '@prisma/client/runtime/library'
 
-import Logger from 'Utils/Logger/Logger'
+import Logger from 'Utils/Logger/Logger.js'
 
 const prisma = new PrismaClient()
 
 Logger.info('DB Init')
+
+try {
+	await prisma.$connect()
+
+	Logger.info('DB Connected')
+} catch (error) {
+	if (error instanceof PrismaClientInitializationError) {
+		Logger.error('Prisma failed to connect to the database.', { error })
+	}
+}
 
 export default prisma
