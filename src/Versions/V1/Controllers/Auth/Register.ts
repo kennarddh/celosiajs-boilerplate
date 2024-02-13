c60f96be3254a217c2af2a06e89bd11d9f314e72
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-13 19:55:36 +0700

    fix: Register

--- src/Versions/V1/Controllers/Auth/Register.ts
+++ src/Versions/V1/Controllers/Auth/Register.ts
@@ -1,32 +1,50 @@
 import { Request, Response } from 'express'
 
-import CreateUser from 'Services/User/Create'
+import argon2 from 'argon2'
+
+import Logger from 'Utils/Logger/Logger.js'
+
+import prisma from 'Database/index.js'
 
 interface IBody {
 	username: string
 	name: string
-	email: string
 	password: string
 }
 
-const Register = (req: Request, res: Response) => {
-	const { username, name, email, password }: IBody = req.body
+const Register = async (req: Request, res: Response) => {
+	const { username, name, password }: IBody = req.body
 
-	CreateUser({ username, name, email, password })
-		.then(({ user }) => {
-			return res.status(201).json({
-				errors: [],
-				data: {
-					id: user._id,
-				},
-			})
+	const hashedPassword = await argon2.hash(password, {
+		hashLength: 64,
+	})
+
+	try {
+		const user = await prisma.user.create({
+			data: {
+				username,
+				name,
+				password: hashedPassword,
+			},
 		})
-		.catch(() => {
-			return res.status(500).json({
-				errors: ['Internal server error'],
-				data: {},
-			})
+
+		return res.status(201).json({
+			errors: [],
+			data: {
+				id: user.id,
+			},
 		})
+	} catch (error) {
+		Logger.error('Register controller failed to create user', {
+			username,
+			error,
+		})
+
+		return res.status(500).json({
+			errors: ['Internal server error'],
+			data: {},
+		})
+	}
 }
 
 export default Register
