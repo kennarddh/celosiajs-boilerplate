91da7604b64376bac265fa5ee3794a3e2d810930
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-13 16:22:17 +0700

    fix: Get user data auth

--- src/Types/Http.d.ts
+++ src/Types/Http.d.ts
@@ -1,5 +1,5 @@
 export interface IUser {
-	id: string
+	id: number
 }
 
 declare global {
--- src/Versions/V1/Controllers/Auth/GetUserData.ts
+++ src/Versions/V1/Controllers/Auth/GetUserData.ts
@@ -2,13 +2,13 @@ import { Request, Response } from 'express'
 
 import Logger from 'Utils/Logger/Logger.js'
 
-import FindUserById from 'Services/User/FindById'
+import prisma from 'Database/index.js'
 
-const GetUserData = (req: Request, res: Response) => {
+const GetUserData = async (req: Request, res: Response) => {
 	const id = req.user?.id
 
 	if (!id) {
-		Logger.error('Undefined id GetUserData controller', {
+		Logger.error('Undefined id in GetUserData controller', {
 			id,
 		})
 
@@ -18,34 +18,46 @@ const GetUserData = (req: Request, res: Response) => {
 		})
 	}
 
-	FindUserById({ id })
-		.then(({ user }) => {
-			Logger.info('Get user data success', {
-				id: user._id,
-			})
-
-			return res.status(200).json({
-				errors: [],
-				data: {
-					id: user._id,
-					username: user.username,
-					name: user.name,
-					email: user.email,
-				},
-			})
+	try {
+		const user = await prisma.user.findFirst({
+			where: { id },
+			select: {
+				id: true,
+				username: true,
+				name: true,
+			},
 		})
-		.catch(({ code }) => {
-			if (code === 404) {
-				Logger.info('Get user data not found user', {
-					id,
-				})
-			}
+
+		if (!user) {
+			Logger.info("Can't find user in GetUserData controller", {
+				id,
+			})
 
 			return res.status(500).json({
 				errors: ['Internal server error'],
 				data: {},
 			})
+		}
+
+		return res.status(200).json({
+			errors: [],
+			data: {
+				id: user.id,
+				username: user.username,
+				name: user.name,
+			},
 		})
+	} catch (error) {
+		Logger.info('GetUserData controller failed to get user', {
+			id,
+			error,
+		})
+
+		return res.status(500).json({
+			errors: ['Internal server error'],
+			data: {},
+		})
+	}
 }
 
 export default GetUserData
