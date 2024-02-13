852d9f956b43a6a3e0f97fda253391127faabd08
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-02-13 13:37:48 +0700

    feat: Reset migrations into new one migration

--- prisma/migrations/20240213063446_init/migration.sql
+++ prisma/migrations/20240213063446_init/migration.sql
@@ -1,14 +1,12 @@
 -- CreateTable
 CREATE TABLE `User` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `username` VARCHAR(32) NOT NULL,
-    `name` VARCHAR(32) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
+    `username` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(100) NOT NULL,
     `password` VARCHAR(255) NOT NULL,
     `createdAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `updatedAt` DATETIME(3) NOT NULL,
 
     UNIQUE INDEX `User_username_key`(`username`),
-    UNIQUE INDEX `User_email_key`(`email`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
