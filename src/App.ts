23e6bff8cb3ce598b16d6c51ae774de910c28830
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-06-13 20:10:54 +0700

    fix: Router not used

--- src/App.ts
+++ src/App.ts
@@ -1,6 +1,8 @@
 import Cors from 'Middlewares/Cors'
 import LogHttpRequest from 'Middlewares/LogHttpRequest'
 
+import Router from 'Routes'
+
 import 'Database/index'
 
 import { ExpressInstance } from './Internals'
@@ -10,5 +12,7 @@ const Instance = new ExpressInstance({ strict: true })
 // Middleware
 Instance.useMiddlewares(new Cors())
 Instance.useMiddlewares(new LogHttpRequest())
+
+Instance.useRouters(Router)
 
 export default Instance
