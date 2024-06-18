db98b62e4d321e1fd08a4b1656d87ac6573e3af5
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-06-18 11:44:32 +0700

    fix: Internal stop here symbol constant

--- src/Internals/Constants.ts
+++ src/Internals/Constants.ts
@@ -1,1 +1,9 @@
-export const StopHere = Symbol()
+// A hacky way to make invariant symbol works in typescriot
+// Just Symbol() won't work
+declare const stopHereSymbol: unique symbol
+
+// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+export const StopHere: {
+	[stopHereSymbol]: never
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+} = Symbol('InternalStopHere') as any
