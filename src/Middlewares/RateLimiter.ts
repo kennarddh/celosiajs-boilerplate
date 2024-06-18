7deaab8e113e63c331b0d038904dbac4adc4668b
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-06-18 13:05:26 +0700

    feat: Rate limiter with user id if possible

--- src/Middlewares/RateLimiter.ts
+++ src/Middlewares/RateLimiter.ts
@@ -1,49 +1,90 @@
-import { RateLimiterMemory, RateLimiterRes } from 'rate-limiter-flexible'
+import { RateLimiterAbstract, RateLimiterMemory, RateLimiterRes } from 'rate-limiter-flexible'
 
 import { BaseMiddleware, EmptyObject, ExpressRequest, ExpressResponse, StopHere } from 'Internals'
 
 import Logger from 'Utils/Logger/Logger'
 
-const rateLimiter = new RateLimiterMemory({
+import { JWTVerifiedData } from './VerifyJWT'
+
+const ipRateLimiter = new RateLimiterMemory({
+	keyPrefix: 'ip',
 	points: 100,
 	duration: 60, // In seconds
 })
 
+const userRateLimiter = new RateLimiterMemory({
+	keyPrefix: 'user',
+	points: 60,
+	duration: 60, // In seconds
+})
+
 class RateLimiter extends BaseMiddleware {
+	constructor(
+		private pointsToConsume = 1,
+		private useUserRateLimiterIfPossible = true,
+	) {
+		super()
+	}
+
 	public override async index(
-		_: EmptyObject,
+		data: EmptyObject | JWTVerifiedData,
 		request: ExpressRequest,
 		response: ExpressResponse,
 	) {
+		if ('user' in data && this.useUserRateLimiterIfPossible) {
+			try {
+				const rateLimiterRes = await userRateLimiter.consume(
+					data.user.id,
+					this.pointsToConsume,
+				)
+
+				this.handleRateLimiterRes(response, userRateLimiter, rateLimiterRes)
+
+				return {}
+			} catch (error: unknown) {
+				if (error instanceof RateLimiterRes) {
+					const rateLimiterRes = error
+
+					this.handleRateLimiterRes(response, userRateLimiter, rateLimiterRes)
+
+					response
+						.status(429)
+						.json({ errors: { others: ['Rate limit exceeded'] }, data: {} })
+
+					return StopHere
+				}
+
+				Logger.error('User rate limiter error', error)
+
+				response.status(500).json({
+					errors: { others: ['Internal server error'] },
+					data: {},
+				})
+
+				return StopHere
+			}
+		}
+
 		if (request.ip === undefined) {
 			Logger.warn('Rate limiter undefined ip')
+
+			response.status(500).json({
+				errors: { others: ['Internal server error'] },
+				data: {},
+			})
 
 			return StopHere
 		}
 
 		try {
-			const rateLimiterRes = await rateLimiter.consume(request.ip, 1)
+			const rateLimiterRes = await ipRateLimiter.consume(request.ip, this.pointsToConsume)
 
-			const headers = {
-				'Retry-After': rateLimiterRes.msBeforeNext / 1000,
-				'RateLimit-Limit': rateLimiter.points,
-				'RateLimit-Remaining': rateLimiterRes.remainingPoints,
-				'RateLimit-Reset': Date.now() + rateLimiterRes.msBeforeNext,
-			}
-
-			response.header(headers)
+			this.handleRateLimiterRes(response, ipRateLimiter, rateLimiterRes)
 		} catch (error: unknown) {
 			if (error instanceof RateLimiterRes) {
 				const rateLimiterRes = error
 
-				const headers = {
-					'Retry-After': rateLimiterRes.msBeforeNext / 1000,
-					'RateLimit-Limit': rateLimiter.points,
-					'RateLimit-Remaining': rateLimiterRes.remainingPoints,
-					'RateLimit-Reset': Date.now() + rateLimiterRes.msBeforeNext,
-				}
-
-				response.header(headers)
+				this.handleRateLimiterRes(response, ipRateLimiter, rateLimiterRes)
 
 				response.status(429).json({ errors: { others: ['Rate limit exceeded'] }, data: {} })
 
@@ -61,6 +102,21 @@ class RateLimiter extends BaseMiddleware {
 		}
 
 		return {}
+	}
+
+	private handleRateLimiterRes(
+		response: ExpressResponse,
+		rateLimiter: RateLimiterAbstract,
+		rateLimiterRes: RateLimiterRes,
+	) {
+		const headers = {
+			'Retry-After': rateLimiterRes.msBeforeNext / 1000,
+			'RateLimit-Limit': rateLimiter.points,
+			'RateLimit-Remaining': rateLimiterRes.remainingPoints,
+			'RateLimit-Reset': Date.now() + rateLimiterRes.msBeforeNext,
+		}
+
+		response.header(headers)
 	}
 }
 
