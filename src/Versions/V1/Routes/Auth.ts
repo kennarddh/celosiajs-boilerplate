ffff36d48f648807c4a4636447aa9f2dec071b9f
kennarddh <79390992+kennarddh@users.noreply.github.com>
2024-06-18 13:36:38 +0700

    feat: Rate limit for auth

--- src/Versions/V1/Routes/Auth.ts
+++ src/Versions/V1/Routes/Auth.ts
@@ -1,10 +1,13 @@
 import { ExpressRouter } from 'Internals/ExpressProvider'
 
+import RateLimiter from 'Middlewares/RateLimiter'
 import VerifyJWT from 'Middlewares/VerifyJWT'
 
 import { GetUserData, Login, RefreshToken, Register } from 'Versions/V1/Controllers/Auth/index'
 
 const AuthRouter = new ExpressRouter({ strict: true })
+
+AuthRouter.useMiddlewares(new RateLimiter())
 
 AuthRouter.post('/register', [], new Register())
 AuthRouter.post('/login', [], new Login())
