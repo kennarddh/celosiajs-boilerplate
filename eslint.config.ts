be4e6200e821b73734d08be94a63b6b1b1d8fc45
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-01-12 20:30:12 +0700

    chore: Modify eslint config

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,27 +1,29 @@
 import js from '@eslint/js'
 import { FlatConfig } from '@typescript-eslint/utils/ts-eslint'
-// import importPlugin from 'eslint-plugin-import'
+import eslintPluginImportX from 'eslint-plugin-import-x'
 import prettier from 'eslint-plugin-prettier'
 import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import security from 'eslint-plugin-security'
 import globals from 'globals'
 import tsEslint from 'typescript-eslint'
 
-// TODO: Enable eslint-plugin-import again when it supports flat config by uncommenting all commented lines
-// https://github.com/import-js/eslint-plugin-import/issues/2556
-
-// Also eslint-import-resolver-typescript
-
 export default tsEslint.config(
-	{ ignores: ['build/**/*', 'eslint.config.d.ts', 'eslint.config.js'] },
+	js.configs.recommended,
+	eslintPluginImportX.flatConfigs.recommended,
+	eslintPluginImportX.flatConfigs.typescript,
+	security.configs.recommended,
+	...tsEslint.configs.strictTypeChecked,
+	...tsEslint.configs.stylisticTypeChecked,
+	prettierRecommended,
+	{ ignores: ['**/build/**/*'] },
 	{
 		languageOptions: {
 			parser: tsEslint.parser as FlatConfig.Parser,
 			parserOptions: {
 				ecmaVersion: 'latest',
 				sourceType: 'module',
-				project: 'tsconfig.json',
 				tsconfigRootDir: import.meta.dirname,
+				projectService: true,
 				extraFileExtensions: ['.json'],
 			},
 			globals: {
@@ -30,11 +32,6 @@ export default tsEslint.config(
 			},
 		},
 	},
-	js.configs.recommended,
-	security.configs.recommended,
-	...tsEslint.configs.strictTypeChecked,
-	...tsEslint.configs.stylisticTypeChecked,
-	prettierRecommended,
 	{
 		name: 'Prettier Warn',
 		plugins: {
@@ -47,13 +44,10 @@ export default tsEslint.config(
 	{
 		files: ['**/*.ts', 'eslint.config.ts'],
 		plugins: {
-			// import: importPlugin,
 			'@typescript-eslint': tsEslint.plugin,
 			security,
 		},
 		rules: {
-			// ...importPlugin.configs.recommended.rules,
-			// ...importPlugin.configs.typescript.rules,
 			'@typescript-eslint/no-empty-function': [
 				'error',
 				{ allow: ['private-constructors', 'protected-constructors'] },
@@ -81,51 +75,8 @@ export default tsEslint.config(
 				},
 			],
 			'@typescript-eslint/no-confusing-void-expression': 'off',
-			// 'import/prefer-default-export': 'off',
-			// 'import/extensions': ['warn', { ts: 'never', json: 'never' }],
+			'import-x/extensions': ['warn', { ts: 'never', json: 'never' }],
+			'import-x/no-named-as-default-member': 'off',
 		},
-		// settings: {
-		// 'import/resolver': {
-		// 	typescript: {},
-		// },
-		// },
-	},
-	{
-		files: ['eslint.config.ts', 'eslint.d.ts'],
-		languageOptions: {
-			parserOptions: {
-				project: 'tsconfig.eslint.json',
-			},
-		},
-		plugins: {
-			// import: importPlugin,
-		},
-		rules: {
-			// 'import/no-extraneous-dependencies': [
-			// 	'error',
-			// 	{
-			// 		devDependencies: true,
-			// 	},
-			// ],
-		},
-	},
-	{
-		files: ['scripts/**/*'],
-		languageOptions: {
-			parserOptions: {
-				project: 'tsconfig.dev.json',
-			},
-		},
-		// plugins: {
-		// 	import: importPlugin,
-		// },
-		// rules: {
-		// 	'import/no-extraneous-dependencies': [
-		// 		'error',
-		// 		{
-		// 			devDependencies: true,
-		// 		},
-		// 	],
-		// },
 	},
 )
