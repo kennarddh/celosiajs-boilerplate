2554adfea3e1bb86ff7a7155774fa1ba50d5c0f7
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-01-15 00:56:23 +0700

    chore: Setup migration production docker service to do prisma migrate

---
+++ prisma/migrations/20250114175017_init/migration.sql
@@ -0,0 +1,14 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" VARCHAR(50) NOT NULL,
+    "name" VARCHAR(100) NOT NULL,
+    "password" VARCHAR(255) NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
