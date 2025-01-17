0c6fc8a9cd80f914e0f15573ac913102f48f9a49
kennarddh <79390992+kennarddh@users.noreply.github.com>
2025-01-18 02:35:12 +0700

    chore: Change eslint config and groups specific rules
    Sync with other projects

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,6 +1,5 @@
 import js from '@eslint/js'
-import { FlatConfig } from '@typescript-eslint/utils/ts-eslint'
-import eslintPluginImportX from 'eslint-plugin-import-x'
+import importX from 'eslint-plugin-import-x'
 import prettier from 'eslint-plugin-prettier'
 import prettierRecommended from 'eslint-plugin-prettier/recommended'
 import security from 'eslint-plugin-security'
@@ -9,45 +8,43 @@ import tsEslint from 'typescript-eslint'
 
 export default tsEslint.config(
 	js.configs.recommended,
-	eslintPluginImportX.flatConfigs.recommended,
-	eslintPluginImportX.flatConfigs.typescript,
-	security.configs.recommended,
+	importX.flatConfigs.recommended,
+	importX.flatConfigs.typescript,
 	tsEslint.configs.strictTypeChecked,
 	tsEslint.configs.stylisticTypeChecked,
 	prettierRecommended,
-	{ ignores: ['build/**/*'] },
+	{ name: 'Ignores', ignores: ['build/**/*'] },
 	{
+		name: 'Main',
+		files: ['**/*.ts', 'eslint.config.ts'],
 		languageOptions: {
-			parser: tsEslint.parser as FlatConfig.Parser,
+			parser: tsEslint.parser,
 			parserOptions: {
-				ecmaVersion: 'latest',
-				sourceType: 'module',
+				ecmaVersion: 2025,
 				tsconfigRootDir: import.meta.dirname,
 				projectService: true,
-				extraFileExtensions: ['.json'],
 			},
 			globals: {
 				...globals.node,
-				...globals.es2021,
+				...globals.es2025,
 			},
 		},
-	},
-	{
-		name: 'Prettier Warn',
-		plugins: {
-			prettier,
-		},
-		rules: {
-			'prettier/prettier': 'warn',
-		},
-	},
-	{
-		files: ['**/*.ts', 'eslint.config.ts'],
 		plugins: {
 			'@typescript-eslint': tsEslint.plugin,
 			security,
 		},
 		rules: {
+			...security.configs.recommended.rules,
+			'no-unused-vars': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{
+					argsIgnorePattern: '^_',
+					caughtErrorsIgnorePattern: '^_',
+					destructuredArrayIgnorePattern: '^_',
+					varsIgnorePattern: '^_',
+				},
+			],
 			'@typescript-eslint/no-empty-function': [
 				'error',
 				{ allow: ['private-constructors', 'protected-constructors'] },
@@ -65,18 +62,18 @@ export default tsEslint.config(
 				},
 			],
 			'@typescript-eslint/require-await': 'off',
-			'@typescript-eslint/no-unused-vars': [
-				'warn',
-				{
-					argsIgnorePattern: '^_',
-					caughtErrorsIgnorePattern: '^_',
-					destructuredArrayIgnorePattern: '^_',
-					varsIgnorePattern: '^_',
-				},
-			],
 			'@typescript-eslint/no-confusing-void-expression': 'off',
 			'import-x/extensions': ['warn', { ts: 'never', json: 'never' }],
 			'import-x/no-named-as-default-member': 'off',
+		},
+	},
+	{
+		name: 'Prettier Warn',
+		plugins: {
+			prettier,
+		},
+		rules: {
+			'prettier/prettier': 'warn',
 		},
 	},
 )
